93e01a2d3ab3a7779ea4233cf15c7202e748de1a
Godwinisravel <godwinisravel31@gmail.com>
2025-12-10 21:04:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,8 @@
-const CACHE_NAME = "todo-buzzer-cache-v1";
-const URLS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-  // Add icons if desired:
-  // "./icons/icon-192.png",
-  // "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
+const CACHE_NAME = "todo-cache-v1";
+const URLS_TO_CACHE = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE)));
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
